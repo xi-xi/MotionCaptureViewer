be7bc0e4e67d0ac1b4312bc86eb8a5debdaa45cb
IDlab <idlab.saba@gmail.com>
2015-11-07 08:49:20 +0900

    [add]: add boost link config

--- viewer/src/TrcReader.cpp
+++ viewer/src/TrcReader.cpp
@@ -1,15 +1,17 @@
 #include "TrcReader.hpp"
 #include <string>
+#include <boost/filesystem.hpp>
 namespace viewer{
     TrcReader::TrcReader(){
         
     }
     
     TrcReader::TrcReader(const std::string& filepath){
-        
+        this->open(filepath);
     }
     
     int TrcReader::open(const std::string &filepath){
+        boost::filesystem::path p;
         return 0;
     }
     
